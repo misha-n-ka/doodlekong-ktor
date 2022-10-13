24d494f24709934859564827afbde0d1fc724a27
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-13 12:42:30 +0700

    delete default config

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/plugins/Monitoring.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/plugins/Monitoring.kt
@@ -1,15 +1,8 @@
 package com.plcourse.mkirilkin.plugins
 
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.request.*
 import io.ktor.application.*
-import io.ktor.response.*
+import io.ktor.features.*
 
 fun Application.configureMonitoring() {
-    install(CallLogging) {
-        level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
-    }
-
+    install(CallLogging)
 }
