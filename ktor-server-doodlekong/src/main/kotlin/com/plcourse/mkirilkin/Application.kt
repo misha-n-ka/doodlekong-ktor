33367735fd79772c51f3e5d1a207786d432d9d3e
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-14 09:30:02 +0700

    add configure route for create room

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/Application.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/Application.kt
@@ -4,6 +4,8 @@ import com.plcourse.mkirilkin.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
+val server = DrawingServer()
+
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureSessions()
---
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.plcourse.mkirilkin.util
+
+object Constants {
+
+    const val MAX_ROOM_SIZE = 8
+}
