fcf06c332b0c191f97ae81cfd228b816cb34e328
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-17 11:25:50 +0700

    create phases and handling them

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/data/Room.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/data/Room.kt
@@ -9,6 +9,33 @@ class Room(
     var players: List<Player> = emptyList()
 ) {
 
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+
+    init {
+        setPhaseChangedListener {newPhase ->
+            when(newPhase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
+
     suspend fun broadcast(message: String) {
         players.forEach { player ->
             if (player.socket.isActive) {
@@ -27,5 +54,34 @@ class Room(
 
     fun containsPlayer(username: String): Boolean {
         return players.find { it.userName == username } != null
+    }
+
+    private fun waitingForPlayers() {
+
+    }
+
+    private fun waitingForStart() {
+
+    }
+
+    private fun newRound() {
+
+    }
+
+    private fun gameRunning() {
+
+    }
+
+    private fun showWord() {
+
+    }
+
+
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
