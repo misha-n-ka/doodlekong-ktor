3d0a47b043bffab2a3bb6c7786148b96af9f4e6b
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-17 10:46:56 +0700

    create gson in Application

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/Application.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/Application.kt
@@ -1,10 +1,12 @@
 package com.plcourse.mkirilkin
 
+import com.google.gson.Gson
 import com.plcourse.mkirilkin.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 val server = DrawingServer()
+val gson = Gson()
 
 fun main() {
     embeddedServer(Netty, port = 8001, host = "0.0.0.0") {
