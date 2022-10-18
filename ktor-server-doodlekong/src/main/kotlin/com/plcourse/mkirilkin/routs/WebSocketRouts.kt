10ef6f4ccd73b699af793cdfdcd5766d7037f53a
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-18 11:54:31 +0700

    create DrawData and route for webSocket while game processing

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/routs/WebSocketRouts.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/routs/WebSocketRouts.kt
@@ -1,16 +1,38 @@
 package com.plcourse.mkirilkin.routs
 
 import com.google.gson.JsonParser
+import com.plcourse.mkirilkin.data.Room
 import com.plcourse.mkirilkin.data.models.BaseModel
 import com.plcourse.mkirilkin.data.models.ChatMessage
+import com.plcourse.mkirilkin.data.models.DrawData
 import com.plcourse.mkirilkin.gson
+import com.plcourse.mkirilkin.server
 import com.plcourse.mkirilkin.session.DrawingSession
 import com.plcourse.mkirilkin.util.Constants.TYPE_CHAT_MESSAGE
+import com.plcourse.mkirilkin.util.Constants.TYPE_DRAW_DATA
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.channels.consumeEach
+
+fun Route.gameWebSocketRoute() {
+    route("/ws/draw") {
+        standardWebSocket { socket, clientId, message, payload ->
+            when(payload) {
+                is DrawData -> {
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if (room.phase == Room.Phase.GAME_RUNNING) {
+                        room.broadcastToAllExcept(message, clientId)
+                    }
+                }
+                is ChatMessage -> {
+
+                }
+            }
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (
@@ -35,6 +57,7 @@ fun Route.standardWebSocket(
                     val jsonObject = JsonParser.parseString(message).asJsonObject
                     val type = when (jsonObject.get("type").asString) {
                         TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_DRAW_DATA -> DrawData::class.java
                         else -> BaseModel::class.java
                     }
                     val payload = gson.fromJson(message, type)
