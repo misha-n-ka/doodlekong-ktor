8c322448cb9106fe44d19b78a3bd64a137c6ec0c
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-10-27 10:39:35 +0700

    rename Chat message room to roomName. Add ChatMessage ext for check matching the word

--- ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/data/models/messages/ChatMessage.kt
+++ ktor-server-doodlekong/src/main/kotlin/com/plcourse/mkirilkin/data/models/messages/ChatMessage.kt
@@ -1,12 +1,10 @@
 package com.plcourse.mkirilkin.data.models.messages
 
-import com.plcourse.mkirilkin.data.models.messages.BaseModel
-import com.plcourse.mkirilkin.util.Constants
 import com.plcourse.mkirilkin.util.Constants.TYPE_CHAT_MESSAGE
 
 data class ChatMessage(
     val from: String,
-    val room: String,
+    val roomName: String,
     val message: String,
     val timeStamp: Long
 ) : BaseModel(TYPE_CHAT_MESSAGE)
